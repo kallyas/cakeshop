7d7c1faa1dc389d72acf660b0841d3b02ae8267c
Kally <kallyasmedia@gmail.com>
2020-07-01 08:54:37 +0000

    Added card borders

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log("Js Loaded");
+const users = [
+  {
+    username: "kally",
+    password: "12345",
+  },
+  {
+    username: "hope",
+    password: "12345",
+  },
+  {
+    username: "jane",
+    password: "12345",
+  },
+  {
+    username: "ketty",
+    password: "12345",
+  },
+];
+
+users.push({ username: "vamoos", password: "23454" });
